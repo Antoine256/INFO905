c5ea2dd45e4184a62e3d96f53adba719e3ed13b2
Stroguytriu <dupasquierantoine@gmail.com>
2025-02-10 11:17:39 +0100

    progress bar

--- flirty-or-not-web/src/app/home/home.component.ts
+++ flirty-or-not-web/src/app/home/home.component.ts
@@ -6,10 +6,14 @@ import { PopupComponent } from '../popup/popup.component';
   standalone: true,
   imports: [PopupComponent],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'] 
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  percentage = 0;
+
   onButtonClick() {
     console.log('Button clicked!');
+    this.percentage = Math.floor(Math.random() * 100);
   }
 }
